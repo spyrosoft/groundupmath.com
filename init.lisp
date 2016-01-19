e4f19d05151da39150638e8b99ac4b129c696108
spyrosoft <meta@spyrosoft.com>
2016-01-19 13:33:29 -0800

    Convert static HTML to Common Lisp - Hunchentoot.

---
+++ init.lisp
@@ -0,0 +1,25 @@
+(ql:quickload :hunchentoot)
+(ql:quickload :cl-who)
+
+(defpackage :groundupmath
+  (:use :common-lisp
+        :cl-who
+        :hunchentoot
+        ))
+
+(in-package :groundupmath)
+
+;; Needed if you set :error-template-directory in the easy-acceptor
+(setf hunchentoot::*show-lisp-errors-p* t)
+
+(defvar groundupmath-server
+  (make-instance 'hunchentoot:easy-acceptor
+                 :document-root "."
+                 :error-template-directory "static/error-templates/"
+                 :access-log-destination "logs/access.log"
+                 :message-log-destination "logs/error.log"
+                 :port 8083))
+
+(load "static.lisp")
+
+(hunchentoot:start groundupmath-server)
